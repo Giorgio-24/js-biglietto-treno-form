ba03fa83ef28cfab03541cc3c0f37f750f218272
Giorgio Andrei <cipuandrei@libero.it>
2021-07-09 09:13:45 +0200

    bug-fixed

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/* Quali campi inserire nel form:
-Una casella di testo per il form del passeggero
-Una casella numerica per i km
-Una tendina per selezionare la fascia d'età (Maggiorenne, Minorenne, Over65)
-NON E' NECESSARIA ALCUNA VALIDAZIONE
-Come calcolare  il prezzo:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali,
-Cosa stampare sul biglietto:
-Nome passeggero
-Codice treno (inseriamo una stringa a caso, per ora)
-Numero carrozza (randomizziamo una carrozza da 1 a 12)
-Prezzo (stampato con due decimali)
-Tariffa applicata all'utente (Tariffa minori, Tariffa ordinaria, Tariffa Over65) */
-
 //=VARIABLES.
 var passengerName = document.getElementById('passenger-name');
 
@@ -37,28 +20,24 @@ var printPrice = document.getElementById('print-price');
 //=PRINT TICKET DETAILS.
 var generateTicketButton = document.getElementById('generate-ticket').addEventListener("click",
     function () {
-        passengerName = passengerName.value;
 
-        travelDistance = travelDistance.value;
-
-        ageRange += ageRange.value;
 
         var ticketPrice = travelDistance * 0.21;
 
         //?METTO UN IF PER DETERMINARE IL PREZZO SE HO UNO SCONTO.
-        if (ageRange.value = 'underage') {
+        if (ageRange.value == 'underage') {
             ticketPrice *= 0.8;
             discount = 'Sconto minorenne.';
-        } else if (ageRange.value = 'over-65') {
+        } else if (ageRange.value == 'over-65') {
             ticketPrice *= 0.6;
             discount = 'Sconto over 65.';
-        } else if (ageRange.value = 'adult') {
+        } else if (ageRange.value == 'adult') {
             ticketPrice *= 1;
             discount = 'Tariffa ordinaria.';
         }
 
         //?RIEMPIO I TAG HTML CON I VALORI DEGLI IMPUT INSERITI DALL'UTENTE
-        printName.innerHTML = passengerName;
+        printName.innerHTML = passengerName.value;
 
         printDiscount.innerHTML = discount;
 
@@ -74,7 +53,9 @@ var generateTicketButton = document.getElementById('generate-ticket').addEventLi
     }
 );
 
-var undoButton = document.getElementById('undo').addEventListener("click",
+var undoButton = document.getElementById('undo');
+
+undoButton.addEventListener("click",
     function () {
         passengerName.value = '';
 
